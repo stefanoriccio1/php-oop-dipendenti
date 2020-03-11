046552cf6c89b7baeb420b8589a1e7e83d7750f4
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-11 22:10:53 +0100

    up

--- classes/Dipendente.php
+++ classes/Dipendente.php
@@ -20,9 +20,28 @@
           $this->cf = $_cf;
           $this->reparto = $_reparto;
 
+          if(empty($_nome) || !is_string($_nome)){
+
+            die('inserisci il nome correttamente e solo in lettere');
+
+          }
+          if(empty($_cognome) || !is_string($_cognome)){
+
+            die('inserisci il conome correttamente e solo in lettere');
+
+          }
+          if(empty($_reparto) || !is_string($_reparto)){
+
+            die('inserisci il reparto correttamente e solo in lettere');
+
+          }
+          if(empty($_date) || !is_string($_nome)){
+
+            die('inserisci il nome correttamente e solo in lettere');
+
+          }
+
       }
   }
-
-  echo(Dipendente);
 
 ?>
