4a780a1112517d9c380125c309e24923317fe5bb
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-12 10:44:01 +0100

    up

--- classes/Dipendente.php
+++ classes/Dipendente.php
@@ -3,27 +3,28 @@
 
   class Dipendente {
 
-    public $id;
-    public $nome;
-    public $cognome;
-    public $eta;
-    public $dataNascita;
-    public $cf;
-    public $stipendio;
-    public $reparto;
+    protected $id;
+    protected $nome;
+    protected $cognome;
+    protected $eta;
+    protected $dataNascita;
+    protected $cf;
+    private $stipendio;
+    protected $reparto;
 
-      function __construct($_nome, $_cognome, $_dataNascita, $_cf, $_reparto){
+      public function __construct($_nome, $_cognome, $_dataNascita, $_cf, $_reparto){
 
-          $this->nome = $_nome;
+
           $this->cognome = $_cognome;
           $this->dataNascita = $_dataNascita;
           $this->cf = $_cf;
           $this->reparto = $_reparto;
 
-          if(empty($_nome) || !is_string($_nome)){
-
-            die('inserisci il nome correttamente e solo in lettere');
-
+          if(empty($_nome) || !is_string($_nome) || strlen($_nome) < 3){
+            throw new Exception ('inserisci il nome correttamente e solo in lettere');
+          }
+          else{
+            $this->nome = $_nome;
           }
           if(empty($_cognome) || !is_string($_cognome)){
 
@@ -38,9 +39,35 @@
           if(empty($_date) || !is_string($_nome)){
 
             die('inserisci il nome correttamente e solo in lettere');
-
           }
 
+      }
+
+      public function setCf($_cf){
+        // fai check su dato
+        $this->cf = $_cf;
+      }
+
+      public function getCf(){
+        return $this->cf;
+      }
+
+      public function setEta($_eta){
+        // fai check su dato
+        $this->eta = $_eta;
+      }
+
+      public function getEta(){
+        return $this->eta;
+      }
+
+      public function calcStipendio($_gg, $_prezzoBase){
+
+        $this->stipendio = $_gg * $_prezzoBase;
+      }
+
+      public function getStipendio(){
+        return $this->stipendio;
       }
   }
 
